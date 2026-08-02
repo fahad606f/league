6125140c62a274ef7e3d3bba85dff2c287435de1
fahad606f <by9rbcfthk@privaterelay.appleid.com>
2026-08-02 23:10:30 +0300

    Update data.js

--- data.js
+++ data.js
@@ -22,7 +22,7 @@ const DB = {
         { name: "علي", image: "images/IMG_0452.jpeg", goals: 3, assists: 1, mvps: 0 },
         { name: "فارس", image: "images/IMG_0451.jpeg", goals: 6, assists: 1, mvps: 0 },
         { name: "مهند", image: "images/IMG_0446.jpeg", goals: 2, assists: 2, mvps: 0 },
-        { name: "محمد فهد", image: "images/IMG_0482.jpeg", goals: 3, assists: 1, mvps: 0 },
+        { name: "محمد فهد", image: "images/IMG_0482.jpeg", goals: 4, assists: 1, mvps: 0 },
         { name: "مشاري", image: "images/IMG_0444.jpeg", goals: 3, assists: 4, mvps: 0 },
         { name: "فهد اليامي", image: "images/IMG_9999999.jpeg", goals: 1, assists: 4, mvps: 0 },
         { name: "مشعل", image: "images/IMG_0479.jpeg", goals: 0, assists: 0, mvps: 0 },
@@ -31,8 +31,8 @@ const DB = {
         { name: "سامي", image: "images/IMG_0478.jpeg", goals: 2, assists: 3, mvps: 0 },
         { name: "مؤيد", image: "images/IMG_0481.jpeg", goals: 1, assists: 0, mvps: 0 },
         { name: "وليد الحماد", image: "images/IMG_0481.jpeg", goals: 1, assists: 0, mvps: 0 },
-               { name: "عبدالله", image: "images/IMG_0481.jpeg", goals: 3, assists: 2, mvps: 0 }, 
-        { name: "صنهات", image: "images/IMG_0477.jpeg", goals: 0, assists: 0, mvps: 0 }
+         { name: "عبدالله", image: "images/IMG_0481.jpeg", goals: 3, assists: 2, mvps: 0 }, 
+        { name: "صنهات", image: "images/IMG_0477.jpeg", goals: 0, assists: 1, mvps: 0 }
     ],
 
     matches: [
