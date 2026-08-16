9797203dd463b240a0b9df872f9b1c54fbb59adf
fahad606f <by9rbcfthk@privaterelay.appleid.com>
2026-08-16 23:58:11 +0300

    Update data.js

--- data.js
+++ data.js
@@ -24,7 +24,7 @@ const DB = {
         { name: "مهند", image: "images/IMG_0446.jpeg", goals: 3, assists: 4, mvps: 0 },
         { name: "محمد فهد", image: "images/IMG_0482.jpeg", goals: 6, assists: 1, mvps: 0 },
         { name: "مشاري", image: "images/IMG_0444.jpeg", goals: 5, assists: 5, mvps: 0 },
-        { name: "فهد اليامي", image: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png", goals: 6, assists: 8, mvps: 2 },
+        { name: "فهد اليامي", image: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png", goals: 6, assists: 8, mvps: 3 },
         { name: "مشعل", image: "images/IMG_0479.jpeg", goals: 0, assists: 0, mvps: 0 },
         { name: "راكان", image: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png", goals: 1, assists: 0, mvps: 0 },
         { name: "زيد", image: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png", goals: 2, assists: 0, mvps: 0 },
